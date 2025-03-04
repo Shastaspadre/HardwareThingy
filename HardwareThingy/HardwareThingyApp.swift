2ee653fa525be73a8d61362d847c5c8d420eeda8
doomer <john.dumais@comcast.net>
2025-03-04 16:45:57 -0700

    Initial Commit

---
+++ HardwareThingy/HardwareThingyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HardwareThingyApp.swift
+//  HardwareThingy
+//
+//  Created by John Dumais on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct HardwareThingyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HardwareThingyTests/HardwareThingyTests.swift
@@ -0,0 +1,17 @@
+//
+//  HardwareThingyTests.swift
+//  HardwareThingyTests
+//
+//  Created by John Dumais on 3/4/25.
+//
+
+import Testing
+@testable import HardwareThingy
+
+struct HardwareThingyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
