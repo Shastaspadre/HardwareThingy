ee69c7e98fd450bc890492ce7227d5c777d4773d
Robert Dates <rsndates@yahoo.com>
2025-07-16 10:50:24 -0700

    Updated files for viewModelTestablility - Updated Hardware connection View to use task - Updated hardware provider protocol to use async - Updated hardware provider to be able to send new hardware connection errors - Added tests - Some TODOs added     - Translations     - Logging for better state tracking and increase testing coverage - Viewmodel now testable through dependency injection and better thread management

--- HardwareThingy/Services/HardwareProvider.swift
+++ HardwareThingy/Services/HardwareProvider.swift
@@ -9,6 +9,23 @@ import Foundation
 import Combine
 
 class HardwareProvider: HardwareProviderProtocol {
+    func connectToHardware() async throws -> HardwareConnectionState {
+        
+        hardwareConnectionStateSubject.send(.connecting)
+        
+        try await Task.sleep(for: .seconds(2))
+        
+        let randomInt = Int.random(in: 0...5) // Gives provider ability to simulate connection errors
+        if randomInt <= 3 {
+            let cases = HardwareConnectionError.allCases
+            throw cases[randomInt]
+        }
+        
+        hardwareConnectionStateSubject.send(.connecting)
+        
+        return .connected
+    }
+    
     private var hardwareConnectionStateSubject = CurrentValueSubject<HardwareConnectionState, Error>(.unknown)
     lazy var hardwareConnectionStatePublisher = hardwareConnectionStateSubject.eraseToAnyPublisher()
     
---
+++ HardwareThingyTests/Mocks/MockHardwareProvider.swift
@@ -0,0 +1,34 @@
+//
+//  MockHardwareProvider.swift
+//  HardwareThingyTests
+//
+//  Created by Robert Dates on 7/16/25.
+//
+
+import Combine
+import Testing
+import XCTest
+@testable import HardwareThingy
+
+struct MockHardwareProvider: HardwareProviderProtocol {
+    
+    private let subject: CurrentValueSubject<HardwareThingy.HardwareConnectionState, Error> = .init(.unknown)
+    var hardwareConnectionStatePublisher: AnyPublisher<HardwareThingy.HardwareConnectionState, any Error> {
+        subject.eraseToAnyPublisher()
+    }
+    
+    var stubConnectionState: HardwareThingy.HardwareConnectionState
+    var stubError: HardwareConnectionError?
+    
+    init(stubConnectionState: HardwareThingy.HardwareConnectionState, error: HardwareConnectionError? = nil) {
+        self.stubConnectionState = stubConnectionState
+        self.stubError = error
+    }
+    
+    func connectToHardware() async throws -> HardwareThingy.HardwareConnectionState {
+        if let stubError {
+            throw stubError
+        }
+        return stubConnectionState
+    }
+}
